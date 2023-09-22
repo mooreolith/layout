f42d15b4c685580822c1cd678a9062b5e0afb67b
Josh M. Moore <mooreolith@gmail.com>
2023-09-22 12:34:52 -0700

    worked on loss function

--- src/loss.mjs
+++ src/loss.mjs
@@ -2,37 +2,31 @@ import Value from './Value.mjs'
 
 function loss(model, Xb, yb){
   let inputs = Xb.map(input => new Value(input))
-
-  // forward the model to get scores
-  let scores = inputs.map(input => model.eval(input))
-
-  // svm "max-margin" loss
-  let losses = yb.map((yi, i) => {
-    let scorei = scores[i]
-    return new Value(yi).neg().mul(scorei).add(1).relu()
+  let scores = inputs.map(x => model.eval(x))
+  
+  let losses = scores.map((_, i) => {
+    let yi = new Value(yb[i])
+    let si = scores[i]
+    let out = (yi.neg().mul(si)).add(1).relu()
+    return out
   })
-  let data_loss = losses
-    .reduce((a, sum) => sum.add(a), 0)
-    .mul(new Value(losses.length).pow(-1))
+  let data_loss = losses.reduce((sum, it) => sum.add(it), new Value(0.0)).div(losses.length)
   let alpha = new Value(1e-4)
-  let reg_loss = alpha * 
-    model.parameters()
-    .map(p => p.data)
-    .reduce((a, sum) => sum + (a * a))
-  let total_loss = data_loss + reg_loss
+  let reg_loss = alpha.mul(
+    model.parameters().reduce(
+      (sum, r) => sum.add(
+        r.reduce((s, p) => s.add(p), new Value(0.0))
+      ), 
+      new Value(0.0)))
+  let total_loss = data_loss.add(reg_loss)
 
-  let accuracy = yb.map((a, i) => {
-    let yi = a
-    let scorei = scores[i]
-
-    return yi > 0 == scorei.data > 0
+  let accuracy = yb.map((yi, i) => {
+    return (yi > 0) == (scores[i].data > 0) ? 1 : 0
   })
 
   return [
     total_loss, 
-    accuracy
-      .map(a => a.data)
-      .reduce((a, sum) => sum + a, 0)
+    accuracy.reduce((sum, a) => sum + a, 0) / (accuracy.length)
   ]
 }
 
--- test/loss.mjs
+++ test/loss.mjs
@@ -3,22 +3,24 @@ import loss from '../src/loss.mjs'
 import MLP from '../src/MultilayerPerceptron.mjs'
 
 describe('loss function', function(){
-  it('should compute loss', function(){
-    let xs = [
-      [2.0, 3.0, -1.0],
-      [3.0, -1.0, 0.5],
-      [0.5, 1.0, 1.0],
-      [1.0, 1.0, -1.0]
-    ]
-    let ys = [1.0, -1.0, -1.0, 1.0]
+  let xs = [
+    [2.0, 3.0, -1.0],
+    [3.0, -1.0, 0.5],
+    [0.5, 1.0, 1.0],
+    [1.0, 1.0, -1.0]
+  ]
+  let ys = [1.0, -1.0, -1.0, 1.0]
 
-    let model = new MLP(3, [4, 4, 1])
-    let [total_loss, acc] = loss(model, xs, ys)
+  let model = new MLP(3, [4, 4, 1])
+  let [total_loss, acc] = loss(model, xs, ys)
 
+  it('should compute total_loss (not return undefined or NaN)', function(){
     assert.notEqual(total_loss, undefined)
     assert.notEqual(total_loss.data, NaN)
+  })  
 
+  it('should compute accuracy (not return NaN nor undefined', function(){ 
     assert.notEqual(acc, undefined)
-    assert.notEqual(acc, NaN)
+    assert.notEqual(acc.data, NaN)
   })
 })
